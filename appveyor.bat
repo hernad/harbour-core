b5e73e35cf4014bf9260c05d58d2b1b51bcbddc4
hernad <hernad@bring.out.ba>
2018-03-16 17:58:30 +0100

    4x backslash

--- appveyor.bat
+++ appveyor.bat
@@ -15,7 +15,7 @@ set PATH=C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
 rem Upgrade the MSYS2 platform
 REM bash -lc "pacman --noconfirm --sync --refresh --refresh pacman"
 REM bash -lc "pacman --noconfirm --sync --refresh --refresh --sysupgrade --sysupgrade"
-bash -xlc "pacman --noconfirm -S curl zip unzip"
+bash -xc "pacman --noconfirm -S curl zip unzip"
 
 rem Install required tools
 REM bash -xlc "pacman --noconfirm -S --needed base-devel"
@@ -24,7 +24,7 @@ rem Install the relevant native dependencies
 REM bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-json-c"
 REM bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-glib2"
 REM bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-gobject-introspection"
-bash -xlc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-postgresql mingw-w64-%MSYS2_ARCH%-icu mingw-w64-%MSYS2_ARCH%-curl mingw-w64-%MSYS2_ARCH%-openssl"
+bash -xc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-postgresql mingw-w64-%MSYS2_ARCH%-icu mingw-w64-%MSYS2_ARCH%-curl mingw-w64-%MSYS2_ARCH%-openssl"
 
 
 rem Invoke subsequent bash in the build tree
@@ -33,13 +33,14 @@ REM bash -xlc "cd /c ; curl -LO https://dl.bintray.com/hernad/windows/hbwin.tar.
 REM set PATH=C:\hbwin\bin;%PATH%
 
 rem Build/test scripting
-bash -xlc "set pwd"
- 
-bash -xlc "export HB_ARCHITECTURE=win HB_COMPILER=mingw MINGW_INCLUDE=/usr/%MSYSTEM%/include; export HB_WITH_CURL=${MINGW_INCLUDE} HB_WITH_SSL=${MINGW_INCLUDE} HB_WITH_PGSQL=${MINGW_INCLUDE} HB_INSTALL_PREFIX=/c/projects/harbour-core/harbour HB_VER=${APPVEYOR_REPO_TAG_NAME:=0.0.0}; make ; make install"
+bash -xc "set pwd"
+
+REM 4x backslash to get '\' 
+bash -xc "export HB_ARCHITECTURE=win HB_COMPILER=mingw MINGW_INCLUDE=C:\\\\usr\\%MSYSTEM%\\\\include; export HB_WITH_CURL=${MINGW_INCLUDE} HB_WITH_SSL=${MINGW_INCLUDE} HB_WITH_PGSQL=${MINGW_INCLUDE} HB_INSTALL_PREFIX=/c/projects/harbour-core/harbour HB_VER=${APPVEYOR_REPO_TAG_NAME:=0.0.0}; make ; make install"
 
 
 REM postgresql dlls libpq.dll i kompanija
 REM bash -xlc "curl -LO https://dl.bintray.com/hernad/F18/postgresql_windows_x86_dlls.zip; unzip postgresql_windows_x86_dlls.zip"
 
 
-bash -xlc "zip harbour_${BUILD_ARTIFACT}_${APPVEYOR_REPO_TAG_NAME}.zip -r harbour"
+bash -xc "zip harbour_${BUILD_ARTIFACT}_${APPVEYOR_REPO_TAG_NAME}.zip -r harbour"
