10ec3538bd40b78d330a257c9895f4918183af91
hernad <hernad@bring.out.ba>
2018-03-16 17:22:59 +0100

    %MSYSTEM%

--- appveyor.bat
+++ appveyor.bat
@@ -36,7 +36,7 @@ rem Build/test scripting
 bash -xlc "set pwd"
  
 
-bash -xlc "export HB_ARCHITECTURE=win HB_COMPILER=mingw MINGW_INCLUDE=/usr/${MYS2_ARCH}/include HB_WITH_CURL=${MINGW_INCLUDE} HB_WITH_SSL=${MINGW_INCLUDE} HB_WITH_PGSQL=${MINGW_INCLUDE} HB_INSTALL_PREFIX=/c/projects/harbour-core/harbour HB_VER=${APPVEYOR_REPO_TAG_NAME:=0.0.0}; make ; make install"
+bash -xlc "export HB_ARCHITECTURE=win HB_COMPILER=mingw MINGW_INCLUDE=/usr/%MSYSTEM%/include HB_WITH_CURL=${MINGW_INCLUDE} HB_WITH_SSL=${MINGW_INCLUDE} HB_WITH_PGSQL=${MINGW_INCLUDE} HB_INSTALL_PREFIX=/c/projects/harbour-core/harbour HB_VER=${APPVEYOR_REPO_TAG_NAME:=0.0.0}; make ; make install"
 
 
 REM postgresql dlls libpq.dll i kompanija
