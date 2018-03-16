2dceadce7b1ee08bb63c7a1fda8ef841e6ff0060
hernad <hernad@bring.out.ba>
2018-03-16 18:49:36 +0100

    BUILD_RELEASE 3.4.1

--- appveyor.bat
+++ appveyor.bat
@@ -38,9 +38,7 @@ bash -xc "set pwd"
 REM 4x backslash to get '\' 
 bash -xc "export HB_ARCHITECTURE=win HB_COMPILER=mingw MINGW_INCLUDE=C:\\\\msys64\\\\%MSYSTEM%\\\\include; export HB_WITH_CURL=${MINGW_INCLUDE} HB_WITH_OPENSSL=${MINGW_INCLUDE} HB_WITH_PGSQL=${MINGW_INCLUDE} HB_WITH_ICU=${MINGW_INCLUDE} HB_INSTALL_PREFIX=C:\\\\projects\\\\harbour-core\\\\harbour HB_VER=${APPVEYOR_REPO_TAG_NAME:=0.0.0}; ./win-make.exe ; ./win-make.exe install"
 
-
 REM postgresql dlls libpq.dll i kompanija
 REM bash -xlc "curl -LO https://dl.bintray.com/hernad/F18/postgresql_windows_x86_dlls.zip; unzip postgresql_windows_x86_dlls.zip"
-
 
 bash -xc "zip harbour_${BUILD_ARTIFACT}_${APPVEYOR_REPO_TAG_NAME}.zip -r harbour"
