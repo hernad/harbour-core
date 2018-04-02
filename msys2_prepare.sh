9482610680c6e2932d2e134cdfeacc7c14d882ba
hernad <hernad@bring.out.ba>
2018-04-02 10:58:26 +0200

    msys2_prepare fix

--- msys2_prepare.sh
+++ msys2_prepare.sh
@@ -2,12 +2,13 @@
 
 MINGW=i686
 
-pacman -Sy base-devel mingw-w64-$MINGW-toolchain git
+echo "run install packages:"
+echo pacman -Sy base-devel mingw-w64-$MINGW-toolchain git
 
 export HB_ARCHITECTURE=win
 export HB_COMPILER=mingw
 
-export PATH=/c/msys64/mingw32/bin/$PATH
+export PATH=/c/msys64/mingw32/bin:$PATH
 
 export HB_INSTALL_PREFIX=$(pwd)/harbour
 export PATH=$PATH:$(pwd)/harbour/bin
