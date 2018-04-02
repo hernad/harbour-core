b952bc6d11279a819598cc8e3604a5d4316fe603
hernad <hernad@bring.out.ba>
2018-04-02 16:20:02 +0200

    fix

--- msys2_prepare.sh
+++ msys2_prepare.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-MINGWARCH=i686
+export MINGWARCH=i686
 
 echo "run install packages (arch: $MINGWARCH):"
 echo pacman -Sy base-devel mingw-w64-$MINGWARCH-toolchain git
 echo pacman -Sy mingw-w64-$MINGWARCH-postgresql
 
-export MINGW_INCLUDE=C:\\msys64\\\$MINGWARCH\\include
+export MINGW_INCLUDE="C:\\msys64\\${MINGWARCH}\\include"
+echo $MINGW_INCLUDE
 export HB_WITH_CURL=${MINGW_INCLUDE} HB_WITH_OPENSSL=${MINGW_INCLUDE} HB_WITH_PGSQL=${MINGW_INCLUDE} HB_WITH_ICU=${MINGW_INCLUDE} 
 
 export HB_ARCHITECTURE=win
