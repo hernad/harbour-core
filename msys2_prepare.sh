e340f382a35458e03d951f970376217f17ca10ee
hernad <hernad@bring.out.ba>
2018-04-02 11:01:27 +0200

    node-x86

--- msys2_prepare.sh
+++ msys2_prepare.sh
@@ -11,6 +11,6 @@ export HB_COMPILER=mingw
 export PATH=/c/msys64/mingw32/bin:$PATH
 
 export HB_INSTALL_PREFIX=$(pwd)/harbour
-export PATH=$PATH:$(pwd)/harbour/bin
+export PATH=$PATH:$(pwd)/harbour/bin:$PATH:$(pwd)/node-x86
 
 export HB_USER_CFLAGS=-DHB_TR_LEVEL=5
