e70fbfc48d92e6c9e12803ebbe3922114c3db36f
Ernad Husremovic <hernad@bring.out.ba>
2019-01-11 16:05:13 +0100

    bintray/1

--- upload_bintray.sh
+++ upload_bintray.sh
@@ -5,23 +5,23 @@
 cd "$(dirname "$0")"
 
 BINTRAY_API_KEY=${BINTRAY_API_KEY:-`cat bintray_api_key`}
-
 BINTRAY_OWNER=hernad
 BINTRAY_REPOS=harbour
 BINTRAY_PACKAGE=$1
-BINTRAY_PACKAGE_VER=$2
+BINTRAY_PACKAGE_VER=$BUILD_BUILDNUMBER
 
 #pacman --noconfirm -S curl zip unzip
 #pacman --noconfirm -S --needed mingw-w64-$MINGW_ARCH-postgresql mingw-w64-$MINGW_ARCH-icu mingw-w64-$MINGW_ARCH-curl mingw-w64-$MINGW_ARCH-openssl
 #mkdir artifacts
 #echo "test" > artifacts/test.txt
 
-FILE=${BINTRAY_PACKAGE}_${$BINTRAY_PACKAGE_VER}.zip
+FILE=${BINTRAY_PACKAGE}_${BINTRAY_PACKAGE_VER}.zip
 
 cd artifacts && zip -r -v $FILE .
 
 ls -lh $FILE
 
+set
 echo uploading $FILE to bintray ...
 
 curl -s -T $FILE \
