a21e3a9692d4d803cfc00109639ea749d73bdd99
Ernad Husremovic <hernad@bring.out.ba>
2019-01-11 21:27:07 +0100

    ia32

--- ci-build-linux.sh
+++ ci-build-linux.sh
@@ -2,11 +2,26 @@
 
 set
 
-sudo apt-get update -y
-sudo apt-get install -y g++ gcc libc6 \
-     libx11-dev libpcre3-dev libssl-dev \
-     libncurses5 libstdc++6  libpq-dev lib32z1
 
+if [ "$BUILD_ARCH" == "ia32" ] ; then
+
+   sudo apt-get install -y g++-multilib gcc-multilib libc6:i386 \
+     libx11-dev:i386 libpcre3-dev:i386 libssl-dev:i386 \
+     libncurses5:i386 libstdc++6:i386 lib32stdc++6  libpq-dev:i386 lib32z1
+
+   dpkg -L libpq5:i386
+
+   export HB_USER_CFLAGS=-m32
+   export HB_USER_DFLAGS='-m32 -L/usr/lib32'
+   export HB_USER_LDFLAGS='-m32 -L/usr/lib32'
+   
+
+else
+   sudo apt-get update -y
+   sudo apt-get install -y g++ gcc libc6 \
+      libx11-dev libpcre3-dev libssl-dev \
+      libncurses5 libstdc++6  libpq-dev lib32z1
+fi
 
 echo "install to: $HB_INSTALL_PREFIX"
 
